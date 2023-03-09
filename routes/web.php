b7f00cb5e8f95e24e15d11bf8135f0e32cb9260d
fakhryfernanda <mff023@gmail.com>
2023-03-10 06:45:04 +0700

    Country detail route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Livewire\CountryDetail;
 use App\Http\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class);
+Route::get('/country/{index}', CountryDetail::class);
